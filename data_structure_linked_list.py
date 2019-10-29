0aea335bf356e79b2b65dc37d41474c075694e3b
Mácio Matheus Arruda <macio.matheus.arruda@accenture.com>
2019-10-29 11:48:51 -0300

    Add method get in linked list

--- data_structure_linked_list.py
+++ data_structure_linked_list.py
@@ -1,7 +1,10 @@
 class Node:
-    def __init__(self, data: str):
+    def __init__(self, data):
         self.val = data
         self.next = None
+
+    def __str__(self):
+        return str(self.val)
 
 
 class LinkedList:
@@ -22,6 +25,19 @@ class LinkedList:
 
         self._size = self._size + 1
 
+    def get(self, index: int):
+        if index > (self._size - 1):
+            return ValueError("Index error")
+
+        if self.head:
+            pointer, i = self.head, 0
+            last_node = pointer
+            while pointer.next:
+                if index == i:
+                    return last_node
+                last_node = pointer.next
+                i = i + 1
+
     def __len__(self):
         return self._size
 
@@ -33,4 +49,4 @@ if __name__ == '__main__':
     my_list.append(5)
     my_list.append(7)
 
-    print(len(my_list))
+    print(my_list.get(2))
