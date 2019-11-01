5b390c694037b41c9f8f153fa0ab63ad6a968cd1
Mácio Matheus Arruda <macio.matheus.arruda@accenture.com>
2019-11-01 10:38:02 -0300

    Initial double linked list and lru cache

---
+++ data_structure_doubly_linked_list.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList(object):
+    def __init__(self):
+        self.head = None
+
+    def append(self):
+        pass
